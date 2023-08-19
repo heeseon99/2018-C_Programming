90450e30d2231d6ab1f778162eeebec55a6d91fa
heeseon99 <gmltjs418@gmail.com>
2023-08-19 13:50:14 +0900

    case 이용해 입력한 문자가 모음(a, e, i, o, u)인지 자음인지 출력

---
+++ 2018-C_Programming/CheckConsonantAndVowe.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	char ch;
+
+
+	printf("문자를 입력하시오 : ");
+	scanf("%c", &ch);
+
+	switch (ch) {
+	case 'a':
+	case 'e':
+	case 'i':
+	case 'o':
+	case 'u':
+		printf("모음입니다.\n");
+		break;
+	default:
+		printf("자음입니다.\n");
+	}
+	return 0;
+}
